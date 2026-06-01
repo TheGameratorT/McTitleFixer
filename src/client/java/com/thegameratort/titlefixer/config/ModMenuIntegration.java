bb28f7e76fa34aa4fa5a9f95f94ec5c42277521b
JustAlex <justalex7473@gmail.com>
2026-06-01 01:27:38 -0400

    feat: 26.1

--- src/client/java/com/thegameratort/titlefixer/config/ModMenuIntegration.java
+++ src/client/java/com/thegameratort/titlefixer/config/ModMenuIntegration.java
@@ -2,15 +2,15 @@ package com.thegameratort.titlefixer.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screens.Screen;
 
 @Environment(EnvType.CLIENT)
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return (Screen parent) -> AutoConfig.getConfigScreen(TitleFixerConfig.class, parent).get();
+        return (Screen parent) -> AutoConfigClient.getConfigScreen(TitleFixerConfig.class, parent).get();
     }
 }
